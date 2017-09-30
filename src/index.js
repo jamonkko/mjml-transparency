84ce50338fd44b88068c7ab9e253daf42efd978e
Jarkko Mönkkönen <jamonkko@gmail.com>
2017-09-30 18:21:38 +0300

    Support for generating altText using html-to-text and also rendering of some params

--- src/index.js
+++ src/index.js
@@ -3,13 +3,27 @@ const appRoot          = require('app-root-path')
 const readDirectory    = require('read-directory')
 const templateToMjml   = require('./mjml-transparency')
 const mjmlToHtml       = require('./mjml')
+const htmlToText       = require('html-to-text')
 
-module.exports = (options = {dir: appRoot.resolve('/email/templates')}) => {
+function appendAltText(result, options, templateConfig) {
+  if (options === false || templateConfig.htmlToText === false) return result
+  if (!result.mjml.html) return result;
+  const htmlToTextOptions = templateConfig.htmlToText || options
+  const altText = htmlToText.fromString(result.mjml.html, typeof(htmlToTextOptions) === 'boolean' ? undefined : htmlToTextOptions)
+  return {...result, altText}
+}
+
+module.exports = (options = {dir: appRoot.resolve('/email/templates'), altText: true}) => {
     const templateDir = options.dir
-    const directives = requireDir(templateDir)
+    const templateConfigs  = requireDir(templateDir)
     let templates = readDirectory.sync(templateDir, { filter: '*.mjml' })
-    return (templateName, context) => {
-      const mjml = templateToMjml(templates[templateName], context, directives[templateName])
-      return mjmlToHtml(mjml)
+    return (templateName, data) => {
+      const templateConfig = templateConfigs[templateName]({template: templateName, data})
+      const mjml = templateToMjml(templates[templateName], data, templateConfig.directives)
+      const result = {
+        mjml: mjmlToHtml(mjml),
+        params: templateConfig.params
+      }
+      return appendAltText(result, options.altText, templateConfig)
     }
 }
--- templates/test.js
+++ templates/test.js
@@ -1,11 +1,28 @@
-module.exports = {
-  order: {
-    items: {
-      item: {
-        name: function () { return this.name },
-        price: function () { return this.price },
-        quantity: function () { return this.quantity }
+module.exports = ({data: {order, address}, template: {name}}) => ({
+  params: {
+    email: {
+      to: address,
+      from: 'support@example.com',
+      subject: `Please confirm your order (${order.orderNumber})`
+    }
+  },
+  htmlToText: {
+    format: {
+      heading: function (node, fn, options) {
+          var h = fn(elem.children, options);
+          return '====\n' + h.toUpperCase() + '\n====';
+      }
+    }
+  },
+  directives: {
+    order: {
+      items: {
+        item: {
+          name: function () { return this.name },
+          price: function () { return this.price },
+          quantity: function () { return this.quantity }
+        }
       }
     }
   }
-}
+})

6e5bff7090e88ff2fe8d6ebade3417265c1662e1
Jarkko Mönkkönen <jamonkko@gmail.com>
2018-08-26 16:42:22 +0300

    match to mj-class automatically also

--- src/mjml-transparency.js
+++ src/mjml-transparency.js
@@ -2,6 +2,12 @@ const Transparency = require('transparency')
 const jsdom = require('jsdom')
 const { JSDOM } = jsdom;
 
+const defaultMatcher = Transparency.matcher
+
+Transparency.matcher = function(element, key) {
+  return mjClassMatches(element.el.getAttribute('mj-class'), key) || defaultMatcher(element, key)
+}
+
 module.exports = function(mjml, context={}, directives) {
     if (!context || Object.keys(context).length == 0) return mjml
     const xmlHeader = '<?xml version="1.0" encoding="UTF-8"?>'
@@ -13,4 +19,14 @@ module.exports = function(mjml, context={}, directives) {
       }
     }
     return dom.window.document.getElementsByTagName('mjml')[0].outerHTML;
+}
+
+function isString(value) { 
+  return typeof value === 'string' || value instanceof String
+}
+
+function mjClassMatches(mjClassesAttribute, key) { 
+  if (!mjClassesAttribute || !(isString(mjClassesAttribute))) return false 
+  const mjClasses = mjClassesAttribute.split(' ')
+  return mjClasses.indexOf(key) != -1
 }
